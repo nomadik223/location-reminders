88cb0d504e441e765186d245fd777acec66138cb
Austin Rogers <contactALRogers@gmail.com>
2017-05-01 10:46:41 -0700

    Initial Commit

---
+++ LocationReminders/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocationReminders
+//
+//  Created by Kent Rogers on 5/1/17.
+//  Copyright © 2017 Austin Rogers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocationReminders/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocationReminders
+//
+//  Created by Kent Rogers on 5/1/17.
+//  Copyright © 2017 Austin Rogers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
